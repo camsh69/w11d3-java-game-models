8654ec4aa7b176f18dbb9dd75770456bdb28bc5c
Clau <clau.rjimenez@gmail.com>
2022-02-16 14:44:31 +0000

    Added enum data

--- src/main/java/players/SpellType.java
+++ src/main/java/players/SpellType.java
@@ -1,4 +1,17 @@
 package players;
 
 public enum SpellType {
+    FIREBALL(3),
+    LIGHTNING(3),
+    CYCLONE(4);
+
+    private final int healthPoints;
+
+    SpellType(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
 }
