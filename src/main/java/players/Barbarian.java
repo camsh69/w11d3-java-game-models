188416b8a94cf09a2dc200ba53051078c9febe31
Clau <clau.rjimenez@gmail.com>
2022-02-16 14:44:55 +0000

    Warrior children classes

--- src/main/java/players/Barbarian.java
+++ src/main/java/players/Barbarian.java
@@ -1,4 +1,13 @@
 package players;
 
-//public class Barbarian extends Warrior {
-//}
+public class Barbarian extends Warrior {
+    public Barbarian(String name, int healthPoints, int treasurePoints) {
+        super(name, healthPoints, treasurePoints);
+    }
+
+    @Override
+    WeaponType setStartingWeapon() {
+        addWeapon(WeaponType.WARHAMMER);
+        return WeaponType.WARHAMMER;
+    }
+}
--- src/main/java/players/Dwarf.java
+++ src/main/java/players/Dwarf.java
@@ -1,4 +1,13 @@
 package players;
 
-//public class Dwarf extends Warrior {
-//}
+public class Dwarf extends Warrior {
+    public Dwarf(String name, int healthPoints, int treasurePoints) {
+        super(name, healthPoints, treasurePoints);
+    }
+
+    @Override
+    WeaponType setStartingWeapon() {
+        addWeapon(WeaponType.AXE);
+        return WeaponType.AXE;
+    }
+}
