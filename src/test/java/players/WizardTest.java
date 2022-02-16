d89df4c740c8eec6bc606d04e321f82529473bf2
Clau <clau.rjimenez@gmail.com>
2022-02-16 15:20:56 +0000

    added tests for getts

--- src/test/java/players/WizardTest.java
+++ src/test/java/players/WizardTest.java
@@ -1,4 +1,29 @@
 package players;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 public class WizardTest {
+    Wizard wizard;
+
+    @Before
+    public void before() {
+        wizard = new Wizard("Merlin", 8, 10);
+    }
+
+    @Test
+    public void hasSpells() {
+        assertNotNull(wizard.getSpells());
+        assertFalse(wizard.getSpells().isEmpty());
+    }
+
+    @Test
+    public void hasCreatures() {
+        assertNotNull(wizard.getCreatures());
+        assertFalse(wizard.getCreatures().isEmpty());
+    }
+
 }
