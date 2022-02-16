e545810d1edbb789ee2fd8e9c6aa865686f2aec4
Clau <clau.rjimenez@gmail.com>
2022-02-16 16:20:01 +0000

    heal method

--- src/main/java/characters/Cleric.java
+++ src/main/java/characters/Cleric.java
@@ -1,4 +1,13 @@
 package characters;
 
+import players.Player;
+
 public class Cleric {
+
+    public Cleric() {
+    }
+
+    public void heal(Player player, int treasure) {
+        player.
+    }
 }
--- src/main/java/players/Dwarf.java
+++ src/main/java/players/Dwarf.java
@@ -1,5 +1,7 @@
 package players;
 
+import characters.Cleric;
+
 public class Dwarf extends Warrior {
     public Dwarf(String name, int healthPoints, int treasurePoints) {
         super(name, healthPoints, treasurePoints);
@@ -9,5 +11,10 @@ public class Dwarf extends Warrior {
     WeaponType setStartingWeapon() {
         addWeapon(WeaponType.AXE);
         return WeaponType.AXE;
+    }
+
+    public void getHealed(int treasurePoints, Cleric cleric) {
+//        this is halfway done
+//        cleric.heal(this, treasurePoints);
     }
 }
