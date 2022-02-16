30ef0cb2e1cb8247b1d30ce1ba4fe39f3bd46418
Clau <clau.rjimenez@gmail.com>
2022-02-16 13:34:10 +0000

    added Enemy methods

--- src/main/java/characters/Enemy.java
+++ src/main/java/characters/Enemy.java
@@ -3,11 +3,11 @@ package characters;
 public class Enemy {
     private EnemyType enemyType;
 
-    public EnemyType getEnemyType() {
-        return enemyType;
-    }
-
     public Enemy(EnemyType enemyType) {
         this.enemyType = enemyType;
+    }
+
+    public EnemyType getEnemyType() {
+        return this.enemyType;
     }
 }
