e7ca1667afd07492485a949f069da8677edd66eb
Clau <clau.rjimenez@gmail.com>
2022-02-16 14:44:17 +0000

    Added enum data

--- src/main/java/players/CreatureType.java
+++ src/main/java/players/CreatureType.java
@@ -1,4 +1,17 @@
 package players;
 
 public enum CreatureType {
+    IMP(1),
+    DRAGON(2),
+    GOLEM(2);
+
+    private final int healthPoints;
+
+    CreatureType(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
 }
